b96f6699b88f488235f629c70fefca02c38621f8
Kelvin Zhang <me@kelvinzhang.ca>
2022-12-25 23:34:47 -0800

    add tailwind

--- src/lib/components/Inspector.svelte
+++ src/lib/components/Inspector.svelte
@@ -8,17 +8,31 @@
 </script>
 
 <div class="inspector">
-	<div class="section">
-		<div class="title">Fill</div>
-		<div class="content">
-			<div class="row">
-				<ColorInput bind:color={color} setColor={(newColor) => color = newColor} />
-				<VisibilityToggle bind:visible={fillVisible} setVisible={(visible) => {
-                    fillVisible = visible
-                }} />
-			</div>
-		</div>
-	</div>
+    <div class="sections">
+        <div class="section">
+            <div class="title">Position and Size</div>
+            <div class="content">
+                <div class="row">
+                    <div class="bg-red-100">
+                        <Input label="X" type="text" />
+                    </div>
+                    <div class="col-1-2">b</div>
+                    <!-- <Input label="Y" type="text" /> -->
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="title">Fill</div>
+            <div class="content">
+                <div class="row">
+                    <ColorInput bind:color={color} setColor={(newColor) => color = newColor} />
+                    <VisibilityToggle bind:visible={fillVisible} setVisible={(visible) => {
+                        fillVisible = visible
+                    }} />
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
@@ -31,30 +45,23 @@
 		padding: 16px;
 	}
 
+    .sections {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
 	.title {
 		margin-bottom: 8px;
 	}
 
 	.row {
 		display: flex;
+        flex-basis: 0;
 		gap: 8px;
 	}
 
-	input[type='color'] {
-		-webkit-appearance: none;
-		background: none;
-		border-radius: 8px;
-		border-width: 0;
-		width: 20px;
-		height: 20px;
-	}
-
-	input[type='color']::-webkit-color-swatch-wrapper {
-		padding: 0;
-	}
-
-	input[type='color']::-webkit-color-swatch {
-		border-radius: 4px;
-		border-width: 0;
-	}
+    .col-1-2 {
+        width: 50%;
+    }
 </style>
--- src/lib/components/common/Input.svelte
+++ src/lib/components/common/Input.svelte
@@ -1,25 +1,35 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
-  let inputProps;
-  $: ({ ...inputProps } = $$props);
+	let inputProps;
+	$: ({ label, ...inputProps } = $$props);
 </script>
 
-<div>
-  <input {...inputProps} />
+<div class="input-group">
+    {#if label}
+        <label for={inputProps.id}>{label}</label>
+    {/if}
+	<input {...inputProps} />
 </div>
 
 <style lang="scss">
-    input[type='text'] {
+    .input-group {
+        display: flex;
+        align-items: center;
+        // flex-direction: column;
+        gap: 8px;
+    }
+
+	input[type='text'] {
 		border: 1px solid transparent;
-        border-radius: 8px;
-		background-color: #2B2B2B;
+		border-radius: 8px;
+		background-color: #2b2b2b;
 		color: white;
 		padding: 8px 8px;
-        outline: none;
+		outline: none;
 
-        &:focus {
-            border: 1px solid #1D71AE;
-        }
+		&:focus {
+			border: 1px solid #1d71ae;
+		}
 	}
 </style>
